0ad52532bf351e52a96c3d3261c4fddf90147804
mohammed mesbahi <116631139+mohammedMesbahi@users.noreply.github.com>
2023-06-15 00:13:00 +0100

    commit

--- components/dashborads/admin/searchBars/handicrafts.jsx
+++ components/dashborads/admin/searchBars/handicrafts.jsx
@@ -4,12 +4,17 @@ import Autocomplete from "@mui/material/Autocomplete";
 import useSWR from "swr";
 import axios from "axios";
 import Link from "next/link";
+import { Button, Stack } from "@mui/material";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export function HandicraftSearchBar() {
   const [inputValue, setInputValue] = React.useState("");
-  const { data: { data: options = [] } = {}, error } = useSWR(
+  const {
+    data: { data: options = [] } = {},
+    error,
+    isLoading,
+  } = useSWR(
     inputValue ? `/api/admins/handicrafts?fullName=${inputValue}` : null,
     fetcher
   );
@@ -20,12 +25,13 @@ export function HandicraftSearchBar() {
 
   return (
     <Autocomplete
-    
+      noOptionsText={isLoading ? "Searching..." : inputValue ? "No Match" : ""}
       options={options}
       getOptionLabel={(option) => option.fullName}
       renderOption={(props, option) => (
-        <Link href={`/admin/dashboard/handicrafts/${option._id}`}>
-          <div {...props}>
+        <div {...props} key={option._id} style={{width:'100%'}} >
+          <Stack flexDirection={"row"} gap={1} flexGrow={1} alignItems={"center"}>
+            <Stack flexDirection={'row'} flexGrow={1} alignItems={'center'} gap={1}>
             <img
               src={option.profileImage}
               alt={option.fullName}
@@ -36,8 +42,31 @@ export function HandicraftSearchBar() {
               }}
             />
             {option.fullName}
-          </div>
-        </Link>
+            </Stack>
+            <Stack
+              sx={{
+                flexDirection: "row",
+                alignItems: "center",
+                alignSelf: "flex-end",
+                justifySelf: "flex-end",
+              }}
+              gap={1}
+            >
+              <Button
+                component={Link}
+                href={`/admin/dashboard/handicrafts/${option._id}`}
+                variant="contained"
+                color="info"
+                onClick={() => {}}
+              >
+                view profile
+              </Button>
+              <Button variant="contained" color="secondary" onClick={() => {}}>
+                remove
+              </Button>
+            </Stack>
+          </Stack>
+        </div>
       )}
       onInputChange={(event, newInputValue) => {
         setInputValue(newInputValue);
